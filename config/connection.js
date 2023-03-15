bd935a5ee2533a9ed5861506fa7cd14916cf866b
Leo Wong <leoelicos@gmail.com>
2023-03-15 11:08:32 +1100

    Move connection to external file. Add dotenv

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+import mysql from 'mysql2'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const online = process.env.JAWSDB_URL
+const offline = {
+  port: 3306,
+  host: 'localhost',
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
+}
+
+export default mysql.createConnection(online || offline)

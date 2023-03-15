92609c160ba9b32e3374d9d2b746e20e3392ee8c
Leo Wong <leoelicos@gmail.com>
2023-03-15 11:28:03 +1100

    Try clearDB config
    Hide secrets

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,8 @@ import mysql from 'mysql2'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const online = process.env.JAWSDB_URL
+// const online = process.env.JAWSDB_URL
+const online = process.env.CLEARDB_DATABASE_URL
 const offline = {
   port: 3306,
   host: 'localhost',
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 // Import and require mysql2
 import db from './config/connection.js'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 app.use(cors())
